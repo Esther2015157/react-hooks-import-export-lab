ff77b32d73f0f5cc819b4879e8c88dd18de4576e
Esther2015157 <esthersly8@gmail.com>
2022-07-15 18:24:47 +0300

    imported image from user components to the Home component

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,18 @@
 const username = "Liza";
 const city = "New York";
-
 const image = "https://i.imgur.com/mV8PQxj.gif";
+import { id } from "date-fns/locale";
+// export all three variables as named exports default username;city;
+import image from "../data/user"
+function About() {}
+  return 
+    <div id="about"/>
+    export default username;city;image;
+    
 
-// export all three variables as named exports
+
+
+
+
+
+
